6cd795e72116b002a061ca63021c3a3265b4e023
gustiaryapriandana31 <gustiaryapriandana@gmail.com>
2026-02-27 09:57:57 +0700

    feat:delete button Tambah Role

--- app/Helpers/MenuHelper.php
+++ app/Helpers/MenuHelper.php
@@ -44,7 +44,7 @@ class MenuHelper
                 'name' => 'Kalender',
                 'subItems' => [
                     ['name' => 'Kalender DL', 'path' => '/kalender-dl'],
-                    ['name' => 'Kalender Kegiatan', 'path' => '/kalender-kegiatan'],
+                    // ['name' => 'Kalender Kegiatan', 'path' => '/kalender-kegiatan'],
                 ],
             ],
             [
@@ -52,7 +52,7 @@ class MenuHelper
                 'name' => 'Rencana Kinerja',
                 'subItems' => [
                     ['name' => 'Rencana Kerja Per Fungsi', 'path' => '/master-kegiatan'],
-                    ['name' => 'Rencana Kerja Butuh DL', 'path' => '/rencana-kerja-dl'],
+                    ['name' => 'Rencana Kerja DL', 'path' => '/rencana-kerja-dl'],
                     ],
             ],
         ];
